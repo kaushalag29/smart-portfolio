3c7fe087aa5d3d0e677fbf30d1db4ce3584680a1
kaushalag29 <kaushalag29@gmail.com>
2025-09-06 21:20:31 +0530

    removed tech stack component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,12 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import ThemeToggle from "./ThemeToggle";
-import { Home, Briefcase, BookOpen, Github, FileText, Box } from "lucide-react";
+import { Home, Briefcase, BookOpen, Github, FileText } from "lucide-react";
 
 const navLinks = [
   { name: "Home", href: "/", icon: Home },
   { name: "Projects", href: "/projects", icon: Briefcase },
   { name: "Blog", href: "/blog", icon: BookOpen },
-  { name: "Stack Architect", href: "/tech-stack-architect", icon: Box },
 ];
 
 export default function Navbar() {
